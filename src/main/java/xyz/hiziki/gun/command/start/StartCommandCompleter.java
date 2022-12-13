2c628b51c2c1b53349d474dd16355aca6412af81
hi-kun-jpn <bbremon321@gmail.com>
2022-12-13 16:04:48 +0900

    補完される順番を変更。 ソートが必要なくなったので削除

--- src/main/java/xyz/hiziki/gun/command/start/StartCommandCompleter.java
+++ src/main/java/xyz/hiziki/gun/command/start/StartCommandCompleter.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class StartCommandCompleter implements TabCompleter
@@ -16,10 +15,8 @@ public class StartCommandCompleter implements TabCompleter
         List<String> list = new ArrayList<>(); //List作成
 
         list.add("team");
-        list.add("solo");
         list.add("survival");
-
-        Collections.sort(list); //Listをソート
+        list.add("solo");
 
         return list; //list
     }
