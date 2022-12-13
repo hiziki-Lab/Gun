084601de7ad3cf6a3589ea9d6cca9491be0527ab
hi-kun-jpn <bbremon321@gmail.com>
2022-12-14 01:54:14 +0900

    銃を追加。 これで一応すべての銃が追加されました。

--- src/main/java/xyz/hiziki/gun/guns/GunItemEnum.java
+++ src/main/java/xyz/hiziki/gun/guns/GunItemEnum.java
@@ -2,7 +2,6 @@ package xyz.hiziki.gun.guns;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -11,9 +10,12 @@ public enum GunItemEnum
 {
     NORMAL_GUN(GunItem.Info.AutomaticGun(), 25, 5, 0),
     SHOT_GUN(GunItem.Info.ShotGun(), 6, 5, 0.5),
+    SNIPER_GUN(GunItem.Info.SniperGun(), 4, 6, 5),
+    EXPLODING_GUN(GunItem.Info.ExplodingGun(), 1000, 0, 3),
     FLAME_THROWER_GUN(GunItem.Info.FlameThrowerGun(), 20, 10, 0),
     SEARCH_GUN(GunItem.Info.SearchGun(), 10, 10, 10),
-    POTION_GUN(GunItem.Info.PotionGun(), 3, 10, 1);
+    POTION_GUN(GunItem.Info.PotionGun(), 3, 10, 1),
+    HAND_GUN(GunItem.Info.HandGun(), 12, 4, 0.6);
 
     private final ItemStack gunItemStack; //アイテム情報
 
@@ -53,9 +55,8 @@ public enum GunItemEnum
 
     public static GunItemEnum GetKind(Player Player)
     {
-        PlayerInventory ihv = Player.getInventory();
-        return Arrays.stream(GunItemEnum.values()).filter(v ->
-                Objects.equals(v.getGunItemStack(), ihv.getItemInMainHand())).findFirst().orElse(null);
+        return Arrays.stream(GunItemEnum.values()).filter(v -> Objects.equals(v.getGunItemStack(),
+                Player.getInventory().getItemInMainHand())).findFirst().orElse(null);
     }
 }
 
