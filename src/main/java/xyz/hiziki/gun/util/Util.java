ae6eac6245419272b05474cd77bf02c8acb319b2
hi-kun-jpn <bbremon321@gmail.com>
2022-12-13 16:09:30 +0900

    名前を変更

---
+++ src/main/java/xyz/hiziki/gun/util/Util.java
@@ -0,0 +1,37 @@
+package xyz.hiziki.gun.util;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class Util
+{
+    public static Location under(Location loc)
+    {
+        if (loc.getBlock().getType() != Material.AIR)
+        {
+            return loc;
+        }
+
+        while (loc.getBlock().getType() == Material.AIR)
+        {
+            loc.add(0, -1, 0);
+        }
+        return loc;
+    }
+
+    public static ItemStack itemMeta(Material item, String itemName, String lore1, String lore2, String lore3)
+    {
+        ItemStack stack = new ItemStack(item);
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName(itemName);
+        meta.setLore(Arrays.asList(lore1, lore2, lore3));
+        meta.addEnchant(Enchantment.ARROW_INFINITE, 15, true);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+}
