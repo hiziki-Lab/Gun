e03204d6ce916871cc52bec2b8ddb0ff39b4faa4
hi_kun_JPN <bbremon321@gmail.com>
2022-12-21 23:34:00 +0900

    武器の調整、コーディング規約に従うように

--- src/main/java/xyz/hiziki/gun/guns/GunItem.java
+++ src/main/java/xyz/hiziki/gun/guns/GunItem.java
@@ -20,7 +20,7 @@ import java.util.function.Predicate;
 
 public class GunItem
 {
-    public static void SetGun(Player player)
+    public static void setGun(Player player)
     {
         Inventory inv = player.getInventory();
 
@@ -32,49 +32,49 @@ public class GunItem
 
     public static class Info
     {
-        public static ItemStack AutomaticGun() //自小銃
+        public static ItemStack automaticGun() //自小銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_13, ChatColor.GOLD + "自動小銃",
                     "まゆマークが", "後ろに", "ついている");
         }
 
-        public static ItemStack ShotGun() //散弾銃
+        public static ItemStack shotGun() //散弾銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_CAT, ChatColor.GOLD + "散弾銃",
                     "真ん中の", "まゆさんマークが", "柔らかい");
         }
 
-        public static ItemStack SniperGun() //狙撃銃
+        public static ItemStack sniperGun() //狙撃銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_BLOCKS, ChatColor.GOLD + "狙撃銃",
                     "放たれる弾", "とてつもない早い", "キノコの胞子");
         }
 
-        public static ItemStack ExplodingGun() //爆裂銃
+        public static ItemStack explodingGun() //爆裂銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_CHIRP, ChatColor.GOLD + "爆裂銃(仮)",
                     "この矢には", "世界を滅ぼす", "力がある");
         }
 
-        public static ItemStack FlameThrowerGun() //放射器
+        public static ItemStack flameThrowerGun() //放射器
         {
             return Util.itemMeta(Material.MUSIC_DISC_FAR, ChatColor.GOLD + "火炎放射器",
                     "炎がでてくる", "でもよくみると", "赤いきのこ");
         }
 
-        public static ItemStack SearchGun() //索敵銃
+        public static ItemStack searchGun() //索敵銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_MELLOHI, ChatColor.GOLD + "索敵銃",
                     "アンテナの", "代わりに", "まゆさんヘッド");
         }
 
-        public static ItemStack PotionGun() //散布銃
+        public static ItemStack potionGun() //散布銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_STAL, ChatColor.GOLD + "ポーション散布銃",
-                    "一番", "弱い", "銃かもしれない");
+                    "一番", "強い", "銃かもしれない");
         }
 
-        public static ItemStack HandGun() //小拳銃
+        public static ItemStack handGun() //小拳銃
         {
             return Util.itemMeta(Material.MUSIC_DISC_STRAD, ChatColor.GOLD + "拳銃",
                     "銃ですら", "ないかも", "しれない");
@@ -85,39 +85,39 @@ public class GunItem
     {
         private final static JavaPlugin plugin = Main.getPlugin();
 
-        public static void AutomaticGun(Player player)
+        public static void automaticGun(Player p)
         {
-            player.getWorld().playEffect(player.getLocation(), Effect.BOW_FIRE, 0);
-            Vector vec = player.getEyeLocation().getDirection();
-            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 8F, 2F);
-            arrow.setShooter(player);
+            p.getWorld().playEffect(p.getLocation(), Effect.BOW_FIRE, 0);
+            Vector vec = p.getEyeLocation().getDirection();
+            Arrow arrow = p.getWorld().spawnArrow(p.getLocation().add(0, 2, 0), vec, 6F, 2F);
+            arrow.setShooter(p);
             arrow.setColor(Color.fromBGR((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
-            arrow.setGravity(false);
+            arrow.setGravity(true);
             arrow.setCustomName("AutomaticGun");
 
-            player.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
+            p.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
         }
 
-        public static void ShotGun(Player player) //散弾銃
+        public static void shotGun(Player p) //散弾銃
         {
-            player.getWorld().playEffect(player.getLocation(), Effect.BOW_FIRE, 0);
-            Vector vec = player.getEyeLocation().getDirection();
+            p.getWorld().playEffect(p.getLocation(), Effect.BOW_FIRE, 0);
+            Vector vec = p.getEyeLocation().getDirection();
 
-            for (int shotNo = 0; shotNo < 10; shotNo++)
+            for (int i = 0; i < 10; i++)
             {
-                Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 2F, 25F);
-                arrow.setShooter(player);
+                Arrow arrow = p.getWorld().spawnArrow(p.getLocation().add(0, 2, 0), vec, 2F, 25F);
+                arrow.setShooter(p);
                 arrow.setColor(Color.fromBGR((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
                 arrow.setGravity(false);
                 arrow.setCustomName("ShotGun");
 
-                player.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 20);
+                p.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 20);
             }
         }
 
         private static int count;
 
-        public static void SniperGun(Player player)
+        public static void sniperGun(Player p)
         {
             count = 5;
 
@@ -128,53 +128,48 @@ public class GunItem
                 {
                     if (count == 0)
                     {
-                        shotSniperGun(player);
+                        p.getWorld().playEffect(p.getLocation(), Effect.BOW_FIRE, 0);
+                        Vector vec = p.getEyeLocation().getDirection();
+                        Arrow arrow = p.getWorld().spawnArrow(p.getLocation().add(0, 2, 0), vec, 10F, 0F);
+                        arrow.setShooter(p);
+                        arrow.setColor(Color.BLACK);
+                        arrow.setGravity(false);
+                        arrow.setCustomName("SniperGun");
+
+                        p.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
                         cancel();
                         return;
                     }
                     else if (count == 1)
                     {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 5));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 5));
                     }
                     else if (count == 2)
                     {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 4));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 4));
                     }
                     else if (count == 3)
                     {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 3));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 3));
                     }
                     else if (count == 4)
                     {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 2));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 2));
                     }
                     else if (count == 5)
                     {
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 1));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 1));
                     }
                     count--;
                 }
             }.runTaskTimer(plugin, 0, 20);
         }
 
-        private static void shotSniperGun(Player p)
-        {
-            p.getWorld().playEffect(p.getLocation(), Effect.BOW_FIRE, 0);
-            Vector vec = p.getEyeLocation().getDirection();
-            Arrow arrow = p.getWorld().spawnArrow(p.getLocation().add(0, 2, 0), vec, 16F, 0F);
-            arrow.setShooter(p);
-            arrow.setColor(Color.BLACK);
-            arrow.setGravity(false);
-            arrow.setCustomName("SniperGun");
-
-            p.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
-        }
-
-        public static void ExplodingGun(Player player)
+        public static void explodingGun(Player player)
         {
             player.getWorld().playEffect(player.getLocation(), Effect.BOW_FIRE, 0);
             Vector vec = player.getEyeLocation().getDirection();
-            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 1F, 1F);
+            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 4F, 1F);
             arrow.setShooter(player);
             arrow.setColor(Color.fromBGR((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
             arrow.setGravity(false);
@@ -183,36 +178,36 @@ public class GunItem
             player.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
         }
 
-        public static void FlameThrowerGun(Player player) //火炎放射器
+        public static void flameThrowerGun(Player player) //火炎放射器
         {
             player.getWorld().playEffect(player.getLocation(), Effect.BLAZE_SHOOT, 0);
             Vector vec = player.getEyeLocation().getDirection();
 
-            SmallFireball smallFireball = player.getWorld().spawn(player.getEyeLocation().add(0, 0.5, 0), SmallFireball.class);
-            smallFireball.setShooter(player);
-            smallFireball.setGravity(true);
-            smallFireball.setVelocity(vec);
+            SmallFireball fireball = player.getWorld().spawn(player.getEyeLocation().add(0, 0.5, 0), SmallFireball.class);
+            fireball.setShooter(player);
+            fireball.setGravity(true);
+            fireball.setVelocity(vec);
 
             new BukkitRunnable()
             {
                 @Override
                 public void run()
                 {
-                    SmallFireball smallFireball = player.getWorld().spawn(player.getEyeLocation().add(0, 0.5, 0), SmallFireball.class);
-                    smallFireball.setShooter(player);
-                    smallFireball.setGravity(true);
-                    smallFireball.setVelocity(vec);
+                    SmallFireball fireball = player.getWorld().spawn(player.getEyeLocation().add(0, 0.5, 0), SmallFireball.class);
+                    fireball.setShooter(player);
+                    fireball.setGravity(false);
+                    fireball.setVelocity(vec);
 
-                    player.getServer().getScheduler().runTaskLater(plugin, smallFireball :: remove, 10);
+                    player.getServer().getScheduler().runTaskLater(plugin, fireball :: remove, 10);
                 }
             }.runTaskLater(plugin, 3);
 
-            player.getServer().getScheduler().runTaskLater(plugin, smallFireball :: remove, 10);
+            player.getServer().getScheduler().runTaskLater(plugin, fireball :: remove, 10);
         }
 
-        public static void SearchGun(Player player)
+        public static void searchGun(Player player)
         {
-            List<Block> blockList = player.getLineOfSight(null, 50);
+            List<Block> blockList = player.getLineOfSight(null, 100);
             Location BlockLocation = blockList.get(blockList.size() - 1).getLocation();
             Location targetLocation = Util.under(BlockLocation);
 
@@ -240,14 +235,15 @@ public class GunItem
             }
         }
 
-        public static void PotionGun(Player player) //ポーション散布銃
+        public static void potionGun(Player player) //ポーション散布銃
         {
             player.getWorld().playEffect(player.getLocation(), Effect.BOW_FIRE, 0);
             Vector vec = player.getEyeLocation().getDirection();
-            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 0.5F, 8F);
+            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 1.5F, 4F);
             arrow.setShooter(player);
             arrow.setColor(Color.fromBGR((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
             arrow.setGravity(false);
+            arrow.setCustomName("PotionGun");
 
             player.getServer().getScheduler().runTaskTimer(plugin, () ->
             {
@@ -262,15 +258,16 @@ public class GunItem
                     potion.setShooter(player);
                     arrow.setColor(Color.fromBGR((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
                 }
-            }, 10, 10);
+            }, 5, 5);
+
             player.getServer().getScheduler().runTaskLater(plugin, arrow :: remove, 10 * 20);
         }
 
-        public static void HandGun(Player player)
+        public static void handGun(Player player)
         {
             player.getWorld().playEffect(player.getLocation(), Effect.BOW_FIRE, 0);
             Vector vec = player.getEyeLocation().getDirection();
-            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 2F, 3F);
+            Arrow arrow = player.getWorld().spawnArrow(player.getLocation().add(0, 2, 0), vec, 2F, 2F);
             arrow.setShooter(player);
             arrow.setColor(Color.BLUE);
             arrow.setGravity(false);
