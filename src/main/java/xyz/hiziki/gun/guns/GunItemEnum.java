2e7baf833398eeea530de8971bdb211aa3594dc3
hi_kun_JPN <bbremon321@gmail.com>
2022-12-28 19:08:41 +0900

    爆裂銃廃止、吸収銃に変更

--- src/main/java/xyz/hiziki/gun/guns/GunItemEnum.java
+++ src/main/java/xyz/hiziki/gun/guns/GunItemEnum.java
@@ -11,7 +11,7 @@ public enum GunItemEnum
     AUTOMATIC_GUN(GunItem.Info.automaticGun(), 25, 5, 0),
     SHOT_GUN(GunItem.Info.shotGun(), 6, 5, 0.5),
     SNIPER_GUN(GunItem.Info.sniperGun(), 4, 6, 6),
-    EXPLODING_GUN(GunItem.Info.explodingGun(), 1000, 0, 3),
+    ABSORPTION_GUN(GunItem.Info.absorptionGun(), 20, 5, 0.25),
     FLAME_THROWER_GUN(GunItem.Info.flameThrowerGun(), 20, 10, 0),
     SEARCH_GUN(GunItem.Info.searchGun(), 10, 10, 10),
     POTION_GUN(GunItem.Info.potionGun(), 10, 5, 1),
