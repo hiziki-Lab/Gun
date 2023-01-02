1f889d8579639b4532a176838228cacec2850ffb
hi_kun_JPN <bbremon321@gmail.com>
2023-01-03 02:51:13 +0900

    イベントを追加

--- src/main/java/xyz/hiziki/gun/event/EventManager.java
+++ src/main/java/xyz/hiziki/gun/event/EventManager.java
@@ -4,11 +4,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.hiziki.gun.event.events.EntityDamageByEntity;
-import xyz.hiziki.gun.event.events.PlayerDeath;
-import xyz.hiziki.gun.event.events.PlayerToggleSneak;
+import xyz.hiziki.gun.event.events.*;
 
 public class EventManager implements Listener
 {
@@ -27,6 +27,18 @@ public class EventManager implements Listener
     private void onPlayerDeath(PlayerDeathEvent e)
     {
         new PlayerDeath(e);
+    }
+
+    @EventHandler
+    private void onPlayerInteract(PlayerInteractEvent e)
+    {
+        new PlayerInteract(e);
+    }
+
+    @EventHandler
+    private void onPlayerItemHeld(PlayerItemHeldEvent e)
+    {
+        new PlayerItemHeld(e);
     }
 
     @EventHandler
